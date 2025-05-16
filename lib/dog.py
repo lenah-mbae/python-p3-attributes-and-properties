72aedc7de890a106c10aacfdff8ec18e3acf4cee
Lenah Mbae <lenahmbae22@gmail.com>
2025-05-16 07:45:32 +0300

    finishes test

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,28 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name="Dog", breed="Pug"):
+        self.name = name    # Calls the name setter
+        self.breed = breed  # Calls the breed setter
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    name = property(get_name, set_name)
+
+    def get_breed(self):
+        return self._breed
+
+    def set_breed(self, value):
+        if value in APPROVED_BREEDS:
+            self._breed = value
+        else:
+            print("Breed must be in list of approved breeds.")
+
+    breed = property(get_breed, set_breed)
